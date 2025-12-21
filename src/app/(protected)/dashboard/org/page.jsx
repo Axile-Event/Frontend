4944156f1e46fe889556cea1e446ad70e19c4c7d
oluwatobi <luponet07@gmail.com>
2025-12-21 12:47:55 +0100

    organization dashboard init implementation

---
+++ src/app/(protected)/dashboard/org/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OrganizersDashboardOverview = () => {
+  return (
+    <div>OrganizersDashboardOverview</div>
+  )
+}
+
+export default OrganizersDashboardOverview
