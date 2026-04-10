7e53d4146a9bb16ed2b517b154d1410ec52c2d4f
Abraham123-dev <abrahamoluwaniyi50@gmail.com>
2026-04-10 15:18:15 +0100

    Fix: Update Content Security Policy to include axile.ng in connect-src

--- next.config.mjs
+++ next.config.mjs
@@ -52,7 +52,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: ${trustedHostnames} https://*.googleusercontent.com https://res.cloudinary.com https://*.cloudinary.com; connect-src 'self' ${trustedHostnames} https://nubapi.com https://accounts.google.com https://va.vercel-scripts.com https://vitals.vercel-insights.com; frame-src https://accounts.google.com;`,
+              `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://apis.google.com https://va.vercel-scripts.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: ${trustedHostnames} https://*.googleusercontent.com https://res.cloudinary.com https://*.cloudinary.com; connect-src 'self' ${trustedHostnames} https://axile.ng https://nubapi.com https://accounts.google.com https://va.vercel-scripts.com https://vitals.vercel-insights.com; frame-src https://accounts.google.com;`,
           },
         ],
       },
